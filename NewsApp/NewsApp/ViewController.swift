c12597e20c99d3c660fe6ea2e4cffc34c7b2837d
Adam <adambokun@gmail.com>
2022-02-16 14:58:52 +0300

    add project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Adam Bokun on 16.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
